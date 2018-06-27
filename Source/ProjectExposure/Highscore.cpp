fe2d47b31f213307d1ea99d791f79be49bc88b85
Trym Rannem <trymtrim@live.no>
2018-06-27 12:07:31 +0200

    no message

---
+++ Source/ProjectExposure/Highscore.cpp
@@ -0,0 +1,11 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Highscore.h"
+
+Highscore::Highscore()
+{
+}
+
+Highscore::~Highscore()
+{
+}
---
+++ Source/ProjectExposure/Highscore.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class PROJECTEXPOSURE_API Highscore
+{
+public:
+	Highscore();
+	~Highscore();
+};
